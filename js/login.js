99ce7de78c228fef7f2162f07c8153a78d34e1f9
hasibprangon <hasibulhossainp09@gmail.com>
2024-09-19 15:45:23 +0600

    redirect to home page

--- js/login.js
+++ js/login.js
@@ -1,13 +1,16 @@
 document.getElementById('loginBtn').addEventListener('click',function(event) {
     event.preventDefault()
     const getNumber = document.getElementById('phNumber');
-    const phNumber = getNumber.value;
-    const pinNumber = document.getElementById('pinNumber').value;
-    
+    let phNumber = getNumber.value;
+    let pinNumber = document.getElementById('pinNumber').value;
+   
     if(phNumber === '01566087557' && pinNumber === '123456'){
         console.log('u r logged in');
+        window.location.href = '/homePage.html';
     }
     else{
         alert('Invalid phone number or pin number')
     }
+
+  
 })
