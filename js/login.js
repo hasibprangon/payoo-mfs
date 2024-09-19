731ede8440efe0cb5970313caee8c6c8250b4099
hasibprangon <hasibulhossainp09@gmail.com>
2024-09-19 15:31:13 +0600

    getting phone number and pin number

--- js/login.js
+++ js/login.js
@@ -2,5 +2,12 @@ document.getElementById('loginBtn').addEventListener('click',function(event) {
     event.preventDefault()
     const getNumber = document.getElementById('phNumber');
     const phNumber = getNumber.value;
-    console.log(phNumber);
+    const pinNumber = document.getElementById('pinNumber').value;
+    
+    if(phNumber === '01566087557' && pinNumber === '123456'){
+        console.log('u r logged in');
+    }
+    else{
+        alert('Invalid phone number or pin number')
+    }
 })
