9110caffa2639a87dc968c4483b6f49f7bd278c7
hasibprangon <hasibulhossainp09@gmail.com>
2024-09-19 16:50:03 +0600

    add monye option added

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ document.getElementById('loginBtn').addEventListener('click',function(event) {
     let phNumber = getNumber.value;
     let pinNumber = document.getElementById('pinNumber').value;
    
-    if(phNumber === '01566087557' && pinNumber === '123456'){
+    if(phNumber === '01' && pinNumber === '12'){
         console.log('u r logged in');
         window.location.href = '/homePage.html';
     }
