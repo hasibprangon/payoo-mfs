0b8076ace56c85cf11ed9978741e2cddbaf00035
hasibprangon <hasibulhossainp09@gmail.com>
2024-09-19 19:08:26 +0600

    add money feature added

--- js/home.js
+++ js/home.js
@@ -1,1 +1,24 @@
-console.log('hello');
+document.getElementById('addMoney').addEventListener('click', function (e) {
+    e.preventDefault();
+    // console.log('btn clicked');
+    const getAmountTxt = document.getElementById('addedAmount').value;
+    const getPin = document.getElementById('pinNum').value;
+    const getMainBalance = document.getElementById('mainBalance');
+    const mainBalanceTxt = getMainBalance.innerText;
+    let availableBalance = parseFloat(mainBalanceTxt);
+    const getAmount = parseFloat(getAmountTxt)
+
+    if (getPin === '123') {
+        const newBalance = availableBalance + getAmount;
+        console.log(newBalance);
+        document.getElementById('mainBalance').innerText = newBalance;
+    }
+    else {
+        alert('invalid pin')
+    }
+
+    document.getElementById('addedAmount').value = '' ;
+    document.getElementById('pinNum').value = '';
+})
+
+  
