82cc9be3355bd48a126150c16af9751717c60593
hasibprangon <hasibulhossainp09@gmail.com>
2024-09-19 19:56:58 +0600

    all features added

--- js/features.js
+++ js/features.js
@@ -1,5 +1,14 @@
 document.getElementById('addMoneyOption').addEventListener('click',
-    function() {
+    function () {
         const getAddMoneyOption = document.getElementById('getAddMoney');
-        getAddMoneyOption.classList.remove('hidden')
-    })
+        getAddMoneyOption.classList.remove('hidden');
+        const getCashOutOption = document.getElementById('getCashOut');
+        getCashOutOption.classList.add('hidden')
+    });
+
+document.getElementById('cashOutMoneyOption').addEventListener('click', function () {
+    const getAddMoneyOption = document.getElementById('getAddMoney');
+    getAddMoneyOption.classList.add('hidden');
+    const getCashOutOption = document.getElementById('getCashOut')
+    getCashOutOption.classList.remove('hidden')
+});
